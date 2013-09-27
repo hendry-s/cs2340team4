8215134d96d6e5984688e2053517101f61deee2e
parth2293_thakkar <parth2293_thakkar@hotmail.com>
2013-09-27 20:37:53 +0000

    Person - 4 (Parth)

--- M4/src/edu/gatech/oad/antlab/person/Person4.java
+++ M4/src/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Parthkumar Thakkar
  *  @version 1.1
  */
 public class Person4 {
@@ -30,8 +30,13 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+        String result = "";
+        
+        char first = input.charAt(0);
+    	result = input.substring(1);
+    	result = result + first;
+    	
+      return result;
     }
     
     /**
@@ -45,6 +50,4 @@ public class Person4 {
     public String toString(String input) {
       return name + calc(input);
     }
-
 }
-
