8c648be3fceb1faef09e2452e96ec1aa99f2bb28
nuppan@gmail.com <nuppan@gmail.com@7292638f-3d14-a3d9-2b52-83f5e9dac35b>
2013-10-24 18:38:42 +0000

    Creaated 5 player classes (error present yet)

--- trunk/MuleGame/src/entities/Buzzite.java
+++ trunk/MuleGame/src/entities/Buzzite.java
@@ -1,5 +1,12 @@
 package entities;
 
-public class Buzzite {
+import java.awt.Color;
+
+public class Buzzite extends Player {
+
+	public Buzzite(String name, Color color, String race) {
+		super(name, color, race);
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- trunk/MuleGame/src/entities/Human.java
+++ trunk/MuleGame/src/entities/Human.java
@@ -1,5 +1,14 @@
 package entities;
 
-public class Human {
+import java.awt.Color;
 
+public class Human extends Player {
+
+	public Human(String name, Color color, String race) {
+		super(name, color, race);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
 }
